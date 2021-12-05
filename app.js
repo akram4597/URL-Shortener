3046a07adced490cdaf5345e76b84d140a7900c1
akram4597 <mdakram99@outlook.com>
2021-12-05 22:44:44 +0530

    Added Redirection and click increment  functionality

--- app.js
+++ app.js
@@ -13,11 +13,21 @@ app.use(express.urlencoded({extended:false}))
 app.listen(process.env.PORT || 8000)
 app.use(express.static('public'))
 
-app.get('/', (req, res) =>{
-    res.render('index')
+app.get('/', async (req, res) =>{
+    const shortUrls = await ShortUrl.find()
+    res.render('index',{shortUrls: shortUrls})
 })
 
 app.post('/shortUrls',async (req,res)=>{
     await ShortUrl.create({full:req.body.fullUrl})
     res.redirect('/')
+})
+
+app.get('/:shortUrl', async (req,res) => {
+  const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl})  
+  if(shortUrl == null) return res.sendStatus(404)
+  shortUrl.clicks++
+  shortUrl.save()
+  res.redirect(shortUrl.full)
+
 })
