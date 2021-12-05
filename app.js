a11b2a8d8075d4cb03eb518af565b91d09320d3c
akram4597 <mdakram99@outlook.com>
2021-12-05 21:59:26 +0530

    Added DB connectivity and  Post request functionality

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
+mongoose.connect('mongodb://localhost/urlShortener',{
+    useNewUrlParser: true, useUnifiedTopology: true
+})
+
 app. set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
 
 app.listen(process.env.PORT || 8000)
 app.use(express.static('public'))
@@ -10,3 +17,7 @@ app.get('/', (req, res) =>{
     res.render('index')
 })
 
+app.post('/shortUrls',async (req,res)=>{
+    await ShortUrl.create({full:req.body.fullUrl})
+    res.redirect('/')
+})
